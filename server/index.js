be54b9568bce65e7c7a759b81456f7c2b9c5fffb
Seth Lee <sethlimlee@gmail.com>
2020-06-05 16:25:32 -0600

    takeoff6

--- server/index.js
+++ server/index.js
@@ -1,27 +1,28 @@
-require("dotenv").config();
+// require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const c = require("./controller");
 const massive = require("massive");
 const chalk = require("chalk");
+const path = require("path");
 
-const { SERVER_PORT, CONNECTION_STRING } = process.env;
+// const { SERVER_PORT, CONNECTION_STRING } = process.env;
 
 const app = express();
-
-app.use(bodyParser.json());
-
-massive(CONNECTION_STRING).then((db) => {
-  console.log(chalk.magenta("The Database has Connected!"));
-  app.set("db", db);
-});
-
-const path = require("path");
 // Serve static files from the React frontend app
 app.use(express.static(path.join(__dirname, "client/build")));
 // Anything that doesn't match the above, send back index.html
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
+
+app.use(bodyParser.json());
+
+massive(
+  "postgres://limdvhowehcheu:0ed628baf48e4fdf60a46938539e0eb97e1fc4c415eeaf1ea32bb62150776513@ec2-34-195-169-25.compute-1.amazonaws.com:5432/d10qs21ppq6tfg?ssl=true"
+).then((db) => {
+  console.log(chalk.magenta("The Database has Connected!"));
+  app.set("db", db);
 });
 
 app.get("/api/builds", c.getAllBuilds);
@@ -30,6 +31,6 @@ app.put(
   c.updatebuild
 );
 
-app.listen(SERVER_PORT, () =>
-  console.log(chalk.cyan(`POWER LEVEL OVER ${SERVER_PORT}!!!!!!!`))
+app.listen(3005, () =>
+  console.log(chalk.cyan(`POWER LEVEL OVER ${3005}!!!!!!!`))
 );
