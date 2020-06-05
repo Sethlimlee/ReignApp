671b9fa75eb2b9691410848b9fe9c089ec2f4ad1
Seth Lee <sethlimlee@gmail.com>
2020-06-05 16:58:09 -0600

    heroku deploy fail

--- server/index.js
+++ server/index.js
@@ -4,17 +4,10 @@ const bodyParser = require("body-parser");
 const c = require("./controller");
 const massive = require("massive");
 const chalk = require("chalk");
-const path = require("path");
 
-const { PORT, CONNECTION_STRING } = process.env;
+const { SERVER_PORT, CONNECTION_STRING } = process.env;
 
 const app = express();
-// Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, "reignapp/build")));
-// Anything that doesn't match the above, send back index.html
-app.get("*", (req, res) => {
-  res.sendFile(path.join("app/reignapp/build/index.html"));
-});
 
 app.use(bodyParser.json());
 
@@ -29,6 +22,6 @@ app.put(
   c.updatebuild
 );
 
-app.listen(PORT || 3005, () =>
-  console.log(chalk.cyan(`POWER LEVEL OVER ${PORT}!!!!!!!`))
+app.listen(SERVER_PORT, () =>
+  console.log(chalk.cyan(`POWER LEVEL OVER ${SERVER_PORT}!!!!!!!`))
 );
