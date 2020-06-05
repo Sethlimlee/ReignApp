c19331a848c75c2603682ab4593278453d5f24db
Seth Lee <sethlimlee@gmail.com>
2020-06-05 16:40:35 -0600

    takeoff8

--- server/index.js
+++ server/index.js
@@ -10,10 +10,10 @@ const { PORT, CONNECTION_STRING } = process.env;
 
 const app = express();
 // Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static(path.join(__dirname, "reignapp/build")));
 // Anything that doesn't match the above, send back index.html
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+  res.sendFile(path.join(__dirname + "/reignapp/build/index.html"));
 });
 
 app.use(bodyParser.json());
